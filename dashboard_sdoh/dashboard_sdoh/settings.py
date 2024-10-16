d8b8cfe9ad8b1a7fa185ced3e5d0b1160e0bb7d8
Luis <luisfernandojavier.velazquez@upr.edu>
2024-10-16 14:49:26 -0400

    Update ALLOWED_HOSTS in settings.py

--- dashboard_sdoh/dashboard_sdoh/settings.py
+++ dashboard_sdoh/dashboard_sdoh/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["rpi4.tail78a70.ts.net"]
+ALLOWED_HOSTS = ["rpi4.tail78a70.ts.net", "localhost", "127.0.0.1"]
 
 
 # Application definition
