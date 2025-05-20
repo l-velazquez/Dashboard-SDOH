2670d7f0349036f948832cab6cd7b5ce4204a88b
Luis <luisfernandojavier.velazquez@upr.edu>
2025-05-19 21:35:22 -0400

    Refactor URL patterns for SDOH maps to improve clarity and organization

--- dashboard_sdoh/map/urls.py
+++ dashboard_sdoh/map/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('', views.index, name='index'),  # Main page for map visualization
+    path('', views.sdoh_municipality, name='sdoh_muni'),
     path('puerto_rico/', views.puerto_rico, name='puerto_rico'),  # Puerto Rico specific map
     #path('', views.zipcode_map, name='zipcode'),  # Zipcode map
     path('sdoh/zipcodes', views.sdoh_zip, name='sdoh_zip'),  # SDOH map
-    #path('sdoh/municipalities', views.sdoh_municipality, name='sdoh_muni'),  # Test page
-    path('sdoh/', views.sdoh_municipality, name='sdoh_muni'),  # SDOH map
-    #path('sdoh/', views.sdoh, name='sdoh'),  # SDOH map
+    path('sdoh/municipalities', views.sdoh_municipality, name='sdoh_muni'),  # Test page
+    #path('sdoh/', views.sdoh_municipality, name='sdoh_muni'),  # SDOH map
+    path('sdoh/', views.sdoh, name='sdoh'),  # SDOH map
 ]
