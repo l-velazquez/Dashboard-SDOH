38163864bed62cc1081d18b4c7d09e505eddfb6f
Luis <luisfernandojavier.velazquez@upr.edu>
2025-05-29 14:43:24 -0400

    Refactor settings and URLs for simplified configuration and removed unused components

--- dashboard_sdoh/dashboard_sdoh/urls.py
+++ dashboard_sdoh/dashboard_sdoh/urls.py
@@ -1,7 +1,5 @@
-from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('map.urls')),  # Include URLs from the maps app)
 ]
