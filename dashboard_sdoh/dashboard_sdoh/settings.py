4eb2306b98b1262760b7a9b7124c61f6285e3e8b
Luis <luisfernandojavier.velazquez@upr.edu>
2025-05-29 13:22:21 -0400

    Update database engine to use dummy backend for testing purposes

--- dashboard_sdoh/dashboard_sdoh/settings.py
+++ dashboard_sdoh/dashboard_sdoh/settings.py
@@ -81,10 +81,10 @@ WSGI_APPLICATION = 'dashboard_sdoh.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.dummy'
     }
 }
+
 
 
 # Password validation
