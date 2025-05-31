3b807fa5a2f0ea1c637ff0977c1261940f4fec5c
Luis <luisfernandojavier.velazquez@upr.edu>
2025-05-30 20:15:31 -0400

    Fix ALLOWED_HOSTS assignment to use a list instead of a nested list

--- dashboard_sdoh/dashboard_sdoh/settings.py
+++ dashboard_sdoh/dashboard_sdoh/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 'yes')
-ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', 'localhost').split(',')]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 
 INSTALLED_APPS = [
